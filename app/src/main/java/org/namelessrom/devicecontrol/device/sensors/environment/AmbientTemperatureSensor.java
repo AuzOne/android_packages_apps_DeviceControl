amartinz/DeviceControl

sensors: add sensor information
Environment
  * ambient temperature (°C / °F)
  * light (lx)
  * pressure (hPa / mbar)
  * relative humidity (%)

Motion
  * accelerometer (including gravity)
  * accelerometer linear (excluding gravity)
  * gravity
  * gyroscope
  * gyroscope uncalibrated (jelly bean mr2+)
  * rotation vector
  * step counter (kitkat and above)

Position
  * game rotation vector
  * geomagnetic rotation vector
  * magnetic field sensor (µT)
  * magnetic field sensor uncalibrated (µT, jelly bean mr2+)
  * proximity

Change-Id: I382b6cbafef1fd9db13d020c75713d40e6ec61cf
Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

app/src/main/java/org/namelessrom/devicecontrol/device/sensors/environment/AmbientTemperatureSensor.java
@@ -1,0 +1,68 @@
+/*
+ *  Copyright (C) 2013 - 2015 Alexander "Evisceration" Martinz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.namelessrom.devicecontrol.device.sensors.environment;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.widget.TextView;
+
+import org.namelessrom.devicecontrol.R;
+import org.namelessrom.devicecontrol.device.sensors.BaseSensor;
+
+public class AmbientTemperatureSensor extends BaseSensor {
+    public static final int TYPE = Sensor.TYPE_AMBIENT_TEMPERATURE;
+
+    private Sensor mSensor;
+
+    private TextView mValue;
+
+    @Override public int getImageResourceId() {
+        return R.drawable.ic_walk;
+    }
+
+    @Override public Sensor getSensor() {
+        return mSensor;
+    }
+
+    public AmbientTemperatureSensor(final Context context) {
+        super(context);
+        getInflater().inflate(R.layout.merge_sensor_data_single, getDataContainer(), true);
+
+        mSensor = getSensorManager().getDefaultSensor(TYPE);
+
+        setup(R.string.sensor_temperature);
+
+        mValue = (TextView) findViewById(R.id.sensor_data_single);
+    }
+
+    @Override public void onSensorChanged(SensorEvent event) {
+        if (mValue == null || event.values[0] > Integer.MAX_VALUE) {
+            return;
+        }
+
+        final float value = event.values[0];
+        final float fahrenheit = ((value * 9.0f / 5.0f) + 32.0f);
+        mValue.post(new Runnable() {
+            @Override public void run() {
+                mValue.setText(String.format("%s °C\n%s °F", value, fahrenheit));
+            }
+        });
+    }
+
+}

app/src/main/java/org/namelessrom/devicecontrol/device/sensors/environment/LightSensor.java
@@ -1,0 +1,72 @@
+/*
+ *  Copyright (C) 2013 - 2015 Alexander "Evisceration" Martinz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.namelessrom.devicecontrol.device.sensors.environment;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.widget.TextView;
+
+import org.namelessrom.devicecontrol.R;
+import org.namelessrom.devicecontrol.device.sensors.BaseSensor;
+
+public class LightSensor extends BaseSensor {
+    public static final int TYPE = Sensor.TYPE_LIGHT;
+
+    private Sensor mSensor;
+
+    private TextView mValue;
+
+    @Override public int getImageResourceId() {
+        return R.drawable.ic_walk;
+    }
+
+    @Override public Sensor getSensor() {
+        return mSensor;
+    }
+
+    @Override public int getSensorDelay() {
+        return SensorManager.SENSOR_DELAY_FASTEST;
+    }
+
+    public LightSensor(final Context context) {
+        super(context);
+        getInflater().inflate(R.layout.merge_sensor_data_single, getDataContainer(), true);
+
+        mSensor = getSensorManager().getDefaultSensor(TYPE);
+
+        setup(R.string.sensor_light);
+
+        mValue = (TextView) findViewById(R.id.sensor_data_single);
+    }
+
+    @Override public void onSensorChanged(SensorEvent event) {
+        if (mValue == null || event.values[0] > Integer.MAX_VALUE) {
+            return;
+        }
+
+        final float value = event.values[0];
+        mValue.post(new Runnable() {
+            @Override public void run() {
+                mValue.setText(String.format("%s lx", value));
+            }
+        });
+    }
+
+}

app/src/main/java/org/namelessrom/devicecontrol/device/sensors/environment/RelativeHumiditySensor.java
@@ -1,0 +1,67 @@
+/*
+ *  Copyright (C) 2013 - 2015 Alexander "Evisceration" Martinz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.namelessrom.devicecontrol.device.sensors.environment;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.widget.TextView;
+
+import org.namelessrom.devicecontrol.R;
+import org.namelessrom.devicecontrol.device.sensors.BaseSensor;
+
+public class RelativeHumiditySensor extends BaseSensor {
+    public static final int TYPE = Sensor.TYPE_RELATIVE_HUMIDITY;
+
+    private Sensor mSensor;
+
+    private TextView mValue;
+
+    @Override public int getImageResourceId() {
+        return R.drawable.ic_walk;
+    }
+
+    @Override public Sensor getSensor() {
+        return mSensor;
+    }
+
+    public RelativeHumiditySensor(final Context context) {
+        super(context);
+        getInflater().inflate(R.layout.merge_sensor_data_single, getDataContainer(), true);
+
+        mSensor = getSensorManager().getDefaultSensor(TYPE);
+
+        setup(R.string.sensor_relative_humitidy);
+
+        mValue = (TextView) findViewById(R.id.sensor_data_single);
+    }
+
+    @Override public void onSensorChanged(SensorEvent event) {
+        if (mValue == null || event.values[0] > Integer.MAX_VALUE) {
+            return;
+        }
+
+        final float value = event.values[0];
+        mValue.post(new Runnable() {
+            @Override public void run() {
+                mValue.setText(String.format("%s", value) + " %");
+            }
+        });
+    }
+
+}

app/src/main/java/org/namelessrom/devicecontrol/device/sensors/motion/AccelerometerLinearSensor.java
@@ -1,0 +1,69 @@
+/*
+ *  Copyright (C) 2013 - 2015 Alexander "Evisceration" Martinz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.namelessrom.devicecontrol.device.sensors.motion;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.widget.TextView;
+
+import org.namelessrom.devicecontrol.R;
+import org.namelessrom.devicecontrol.device.sensors.BaseSensor;
+
+public class AccelerometerLinearSensor extends BaseSensor {
+    public static final int TYPE = Sensor.TYPE_LINEAR_ACCELERATION;
+
+    private Sensor mSensor;
+
+    private TextView mValue;
+
+    @Override public int getImageResourceId() {
+        return R.drawable.ic_walk;
+    }
+
+    @Override public Sensor getSensor() {
+        return mSensor;
+    }
+
+    public AccelerometerLinearSensor(final Context context) {
+        super(context);
+        getInflater().inflate(R.layout.merge_sensor_data_single, getDataContainer(), true);
+
+        mSensor = getSensorManager().getDefaultSensor(TYPE);
+
+        setup(R.string.sensor_accelerometer_linear);
+
+        mValue = (TextView) findViewById(R.id.sensor_data_single);
+    }
+
+    @Override public void onSensorChanged(SensorEvent event) {
+        if (mValue == null || event.values[0] > Integer.MAX_VALUE) {
+            return;
+        }
+
+        final float x = event.values[0];
+        final float y = event.values[1];
+        final float z = event.values[2];
+        mValue.post(new Runnable() {
+            @Override public void run() {
+                mValue.setText(String.format("x: %s m/s²\ny: %s m/s²\nz: %s m/s²", x, y, z));
+            }
+        });
+    }
+
+}

app/src/main/java/org/namelessrom/devicecontrol/device/sensors/motion/GyroscopeSensor.java
@@ -1,0 +1,69 @@
+/*
+ *  Copyright (C) 2013 - 2015 Alexander "Evisceration" Martinz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.namelessrom.devicecontrol.device.sensors.motion;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.widget.TextView;
+
+import org.namelessrom.devicecontrol.R;
+import org.namelessrom.devicecontrol.device.sensors.BaseSensor;
+
+public class GyroscopeSensor extends BaseSensor {
+    public static final int TYPE = Sensor.TYPE_GYROSCOPE;
+
+    private Sensor mSensor;
+
+    private TextView mValue;
+
+    @Override public int getImageResourceId() {
+        return R.drawable.ic_walk;
+    }
+
+    @Override public Sensor getSensor() {
+        return mSensor;
+    }
+
+    public GyroscopeSensor(final Context context) {
+        super(context);
+        getInflater().inflate(R.layout.merge_sensor_data_single, getDataContainer(), true);
+
+        mSensor = getSensorManager().getDefaultSensor(TYPE);
+
+        setup(R.string.sensor_gyroscope);
+
+        mValue = (TextView) findViewById(R.id.sensor_data_single);
+    }
+
+    @Override public void onSensorChanged(SensorEvent event) {
+        if (mValue == null || event.values[0] > Integer.MAX_VALUE) {
+            return;
+        }
+
+        final float x = event.values[0];
+        final float y = event.values[1];
+        final float z = event.values[2];
+        mValue.post(new Runnable() {
+            @Override public void run() {
+                mValue.setText(String.format("x: %s rad/s\ny: %s rad/s\nz: %s rad/s", x, y, z));
+            }
+        });
+    }
+
+}

app/src/main/java/org/namelessrom/devicecontrol/device/sensors/position/MagneticFieldSensor.java
@@ -1,0 +1,69 @@
+/*
+ *  Copyright (C) 2013 - 2015 Alexander "Evisceration" Martinz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.namelessrom.devicecontrol.device.sensors.position;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.widget.TextView;
+
+import org.namelessrom.devicecontrol.R;
+import org.namelessrom.devicecontrol.device.sensors.BaseSensor;
+
+public class MagneticFieldSensor extends BaseSensor {
+    public static final int TYPE = Sensor.TYPE_MAGNETIC_FIELD;
+
+    private Sensor mSensor;
+
+    private TextView mValue;
+
+    @Override public int getImageResourceId() {
+        return R.drawable.ic_walk;
+    }
+
+    @Override public Sensor getSensor() {
+        return mSensor;
+    }
+
+    public MagneticFieldSensor(final Context context) {
+        super(context);
+        getInflater().inflate(R.layout.merge_sensor_data_single, getDataContainer(), true);
+
+        mSensor = getSensorManager().getDefaultSensor(TYPE);
+
+        setup(R.string.sensor_magnetic_field);
+
+        mValue = (TextView) findViewById(R.id.sensor_data_single);
+    }
+
+    @Override public void onSensorChanged(SensorEvent event) {
+        if (mValue == null || event.values[0] > Integer.MAX_VALUE) {
+            return;
+        }
+
+        final float x = event.values[0];
+        final float y = event.values[1];
+        final float z = event.values[2];
+        mValue.post(new Runnable() {
+            @Override public void run() {
+                mValue.setText(String.format("x: %s μT\ny: %s μT\nz: %s μT", x, y, z));
+            }
+        });
+    }
+
+}

AuzOne/android_packages_apps_DeviceControl

app/src/main/java/org/namelessrom/devicecontrol/device/sensors/environment/AmbientTemperatureSensor.java
@@ -1,0 +1,68 @@
+/*
+ *  Copyright (C) 2013 - 2015 Alexander "Evisceration" Martinz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.namelessrom.devicecontrol.device.sensors.environment;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.widget.TextView;
+
+import org.namelessrom.devicecontrol.R;
+import org.namelessrom.devicecontrol.device.sensors.BaseSensor;
+
+public class AmbientTemperatureSensor extends BaseSensor {
+    public static final int TYPE = Sensor.TYPE_AMBIENT_TEMPERATURE;
+
+    private Sensor mSensor;
+
+    private TextView mValue;
+
+    @Override public int getImageResourceId() {
+        return R.drawable.ic_heat;
+    }
+
+    @Override public Sensor getSensor() {
+        return mSensor;
+    }
+
+    public AmbientTemperatureSensor(final Context context) {
+        super(context);
+        getInflater().inflate(R.layout.merge_sensor_data_single, getDataContainer(), true);
+
+        mSensor = getSensorManager().getDefaultSensor(TYPE);
+
+        setup(R.string.sensor_temperature);
+
+        mValue = (TextView) findViewById(R.id.sensor_data_single);
+    }
+
+    @Override public void onSensorChanged(SensorEvent event) {
+        if (mValue == null || event.values[0] > Integer.MAX_VALUE) {
+            return;
+        }
+
+        final float value = event.values[0];
+        final float fahrenheit = ((value * 9.0f / 5.0f) + 32.0f);
+        mValue.post(new Runnable() {
+            @Override public void run() {
+                mValue.setText(String.format("%s °C\n%s °F", value, fahrenheit));
+            }
+        });
+    }
+
+}

app/src/main/java/org/namelessrom/devicecontrol/device/sensors/environment/LightSensor.java
@@ -1,0 +1,72 @@
+/*
+ *  Copyright (C) 2013 - 2015 Alexander "Evisceration" Martinz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.namelessrom.devicecontrol.device.sensors.environment;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.widget.TextView;
+
+import org.namelessrom.devicecontrol.R;
+import org.namelessrom.devicecontrol.device.sensors.BaseSensor;
+
+public class LightSensor extends BaseSensor {
+    public static final int TYPE = Sensor.TYPE_LIGHT;
+
+    private Sensor mSensor;
+
+    private TextView mValue;
+
+    @Override public int getImageResourceId() {
+        return R.drawable.ic_brightness;
+    }
+
+    @Override public Sensor getSensor() {
+        return mSensor;
+    }
+
+    @Override public int getSensorDelay() {
+        return SensorManager.SENSOR_DELAY_FASTEST;
+    }
+
+    public LightSensor(final Context context) {
+        super(context);
+        getInflater().inflate(R.layout.merge_sensor_data_single, getDataContainer(), true);
+
+        mSensor = getSensorManager().getDefaultSensor(TYPE);
+
+        setup(R.string.sensor_light);
+
+        mValue = (TextView) findViewById(R.id.sensor_data_single);
+    }
+
+    @Override public void onSensorChanged(SensorEvent event) {
+        if (mValue == null || event.values[0] > Integer.MAX_VALUE) {
+            return;
+        }
+
+        final float value = event.values[0];
+        mValue.post(new Runnable() {
+            @Override public void run() {
+                mValue.setText(String.format("%s lx", value));
+            }
+        });
+    }
+
+}

app/src/main/java/org/namelessrom/devicecontrol/device/sensors/environment/RelativeHumiditySensor.java
@@ -1,0 +1,67 @@
+/*
+ *  Copyright (C) 2013 - 2015 Alexander "Evisceration" Martinz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.namelessrom.devicecontrol.device.sensors.environment;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.widget.TextView;
+
+import org.namelessrom.devicecontrol.R;
+import org.namelessrom.devicecontrol.device.sensors.BaseSensor;
+
+public class RelativeHumiditySensor extends BaseSensor {
+    public static final int TYPE = Sensor.TYPE_RELATIVE_HUMIDITY;
+
+    private Sensor mSensor;
+
+    private TextView mValue;
+
+    @Override public int getImageResourceId() {
+        return R.drawable.ic_humidity;
+    }
+
+    @Override public Sensor getSensor() {
+        return mSensor;
+    }
+
+    public RelativeHumiditySensor(final Context context) {
+        super(context);
+        getInflater().inflate(R.layout.merge_sensor_data_single, getDataContainer(), true);
+
+        mSensor = getSensorManager().getDefaultSensor(TYPE);
+
+        setup(R.string.sensor_relative_humitidy);
+
+        mValue = (TextView) findViewById(R.id.sensor_data_single);
+    }
+
+    @Override public void onSensorChanged(SensorEvent event) {
+        if (mValue == null || event.values[0] > Integer.MAX_VALUE) {
+            return;
+        }
+
+        final float value = event.values[0];
+        mValue.post(new Runnable() {
+            @Override public void run() {
+                mValue.setText(String.format("%s", value) + " %");
+            }
+        });
+    }
+
+}

app/src/main/java/org/namelessrom/devicecontrol/device/sensors/motion/AccelerometerLinearSensor.java
@@ -1,0 +1,69 @@
+/*
+ *  Copyright (C) 2013 - 2015 Alexander "Evisceration" Martinz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.namelessrom.devicecontrol.device.sensors.motion;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.widget.TextView;
+
+import org.namelessrom.devicecontrol.R;
+import org.namelessrom.devicecontrol.device.sensors.BaseSensor;
+
+public class AccelerometerLinearSensor extends BaseSensor {
+    public static final int TYPE = Sensor.TYPE_LINEAR_ACCELERATION;
+
+    private Sensor mSensor;
+
+    private TextView mValue;
+
+    @Override public int getImageResourceId() {
+        return R.drawable.ic_rocket;
+    }
+
+    @Override public Sensor getSensor() {
+        return mSensor;
+    }
+
+    public AccelerometerLinearSensor(final Context context) {
+        super(context);
+        getInflater().inflate(R.layout.merge_sensor_data_single, getDataContainer(), true);
+
+        mSensor = getSensorManager().getDefaultSensor(TYPE);
+
+        setup(R.string.sensor_accelerometer_linear);
+
+        mValue = (TextView) findViewById(R.id.sensor_data_single);
+    }
+
+    @Override public void onSensorChanged(SensorEvent event) {
+        if (mValue == null || event.values[0] > Integer.MAX_VALUE) {
+            return;
+        }
+
+        final float x = event.values[0];
+        final float y = event.values[1];
+        final float z = event.values[2];
+        mValue.post(new Runnable() {
+            @Override public void run() {
+                mValue.setText(String.format("x: %s m/s²\ny: %s m/s²\nz: %s m/s²", x, y, z));
+            }
+        });
+    }
+
+}

app/src/main/java/org/namelessrom/devicecontrol/device/sensors/motion/GyroscopeSensor.java
@@ -1,0 +1,69 @@
+/*
+ *  Copyright (C) 2013 - 2015 Alexander "Evisceration" Martinz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.namelessrom.devicecontrol.device.sensors.motion;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.widget.TextView;
+
+import org.namelessrom.devicecontrol.R;
+import org.namelessrom.devicecontrol.device.sensors.BaseSensor;
+
+public class GyroscopeSensor extends BaseSensor {
+    public static final int TYPE = Sensor.TYPE_GYROSCOPE;
+
+    private Sensor mSensor;
+
+    private TextView mValue;
+
+    @Override public int getImageResourceId() {
+        return R.drawable.ic_compass;
+    }
+
+    @Override public Sensor getSensor() {
+        return mSensor;
+    }
+
+    public GyroscopeSensor(final Context context) {
+        super(context);
+        getInflater().inflate(R.layout.merge_sensor_data_single, getDataContainer(), true);
+
+        mSensor = getSensorManager().getDefaultSensor(TYPE);
+
+        setup(R.string.sensor_gyroscope);
+
+        mValue = (TextView) findViewById(R.id.sensor_data_single);
+    }
+
+    @Override public void onSensorChanged(SensorEvent event) {
+        if (mValue == null || event.values[0] > Integer.MAX_VALUE) {
+            return;
+        }
+
+        final float x = event.values[0];
+        final float y = event.values[1];
+        final float z = event.values[2];
+        mValue.post(new Runnable() {
+            @Override public void run() {
+                mValue.setText(String.format("x: %s rad/s\ny: %s rad/s\nz: %s rad/s", x, y, z));
+            }
+        });
+    }
+
+}

app/src/main/java/org/namelessrom/devicecontrol/device/sensors/position/MagneticFieldSensor.java
@@ -1,0 +1,69 @@
+/*
+ *  Copyright (C) 2013 - 2015 Alexander "Evisceration" Martinz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.namelessrom.devicecontrol.device.sensors.position;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.widget.TextView;
+
+import org.namelessrom.devicecontrol.R;
+import org.namelessrom.devicecontrol.device.sensors.BaseSensor;
+
+public class MagneticFieldSensor extends BaseSensor {
+    public static final int TYPE = Sensor.TYPE_MAGNETIC_FIELD;
+
+    private Sensor mSensor;
+
+    private TextView mValue;
+
+    @Override public int getImageResourceId() {
+        return R.drawable.ic_magnetic;
+    }
+
+    @Override public Sensor getSensor() {
+        return mSensor;
+    }
+
+    public MagneticFieldSensor(final Context context) {
+        super(context);
+        getInflater().inflate(R.layout.merge_sensor_data_single, getDataContainer(), true);
+
+        mSensor = getSensorManager().getDefaultSensor(TYPE);
+
+        setup(R.string.sensor_magnetic_field);
+
+        mValue = (TextView) findViewById(R.id.sensor_data_single);
+    }
+
+    @Override public void onSensorChanged(SensorEvent event) {
+        if (mValue == null || event.values[0] > Integer.MAX_VALUE) {
+            return;
+        }
+
+        final float x = event.values[0];
+        final float y = event.values[1];
+        final float z = event.values[2];
+        mValue.post(new Runnable() {
+            @Override public void run() {
+                mValue.setText(String.format("x: %s μT\ny: %s μT\nz: %s μT", x, y, z));
+            }
+        });
+    }
+
+}